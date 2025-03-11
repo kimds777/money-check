5ae6610a361aeb325b30957ebaa8653b6a9bf610
kimds777 <77198242+kimds777@users.noreply.github.com>
2025-03-11 23:28:01 +0900

    Initialize project with backend and frontend

---
+++ money-check-java/moneycheck/src/main/java/com/money/moneycheck/MoneycheckApplication.java
@@ -0,0 +1,13 @@
+package com.money.moneycheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneycheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneycheckApplication.class, args);
+	}
+
+}
---
+++ money-check-java/moneycheck/src/test/java/com/money/moneycheck/MoneycheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.money.moneycheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneycheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
