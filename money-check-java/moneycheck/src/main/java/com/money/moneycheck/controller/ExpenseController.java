aa4300c2ccc5019c873794f92bd2ae8080d157d8
kimds777 <77198242+kimds777@users.noreply.github.com>
2025-03-16 15:39:59 +0900

    chore: ExpenseController에서  service 접근제어자 수정

--- money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
+++ money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/expenses")
 public class ExpenseController {
 
-    public final ExpenseService expenseService;
+    private final ExpenseService expenseService;
 
     @Autowired
     public ExpenseController(ExpenseService expenseService) {
