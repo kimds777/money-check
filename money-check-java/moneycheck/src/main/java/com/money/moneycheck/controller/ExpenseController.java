40125e7189702c19e01d94c1b188206d58fd1738
kimds777 <77198242+kimds777@users.noreply.github.com>
2025-03-16 15:26:30 +0900

    chore: 지출컨트롤러 이름 및 어노테이션 수정

--- money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
+++ money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
@@ -1,15 +1,27 @@
 package com.money.moneycheck.controller;
 
 
+import com.money.moneycheck.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/expenses")
-public class IndexController {
-    @GetMapping("/")
-    public String index() {
+public class ExpenseController {
+
+    public final ExpenseService expenseService;
+
+    @Autowired
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping("/{user_id}")
+    public String getExpensesByUserId(@PathVariable Long user_id) {
+        expenseService.getExpensesByUserId(user_id);
         return "index";
     }
 }
