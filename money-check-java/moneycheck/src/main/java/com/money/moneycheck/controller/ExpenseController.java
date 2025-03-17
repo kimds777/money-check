77b56b15b379791363667533e3e99f4b0518c72a
kimds777 <77198242+kimds777@users.noreply.github.com>
2025-03-17 21:53:37 +0900

    Feat: 유저별 지출내역 조회 구현

--- money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
+++ money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
@@ -1,12 +1,16 @@
 package com.money.moneycheck.controller;
 
 
+import com.money.moneycheck.domain.Expense;
 import com.money.moneycheck.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/expenses")
@@ -20,8 +24,9 @@ public class ExpenseController {
     }
 
     @GetMapping("/{user_id}")
-    public String getExpensesByUserId(@PathVariable Long user_id) {
-        expenseService.getExpensesByUserId(user_id);
-        return "index";
+    public Optional<Expense> getExpensesByUserId(@PathVariable("user_id") Long userId) {
+        Optional<Expense> expenseList = expenseService.getExpensesByUserId(userId);
+
+        return expenseList;
     }
 }
