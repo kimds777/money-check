84b6e1a7f117705f0ea799b43002c71eccc48cb2
Daseul Kim <tina27849@gmail.com>
2025-04-11 17:15:32 +0900

    feat: 지출 리스트 조회 작업중

--- money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
+++ money-check-java/moneycheck/src/main/java/com/money/moneycheck/controller/ExpenseController.java
@@ -4,6 +4,7 @@ package com.money.moneycheck.controller;
 import com.money.moneycheck.domain.Expense;
 import com.money.moneycheck.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +24,19 @@ public class ExpenseController {
         this.expenseService = expenseService;
     }
 
-    @GetMapping("/{userId}")
-    public Optional<Expense> getExpensesByUserId(@PathVariable("userId") Long userId) {
-        Optional<Expense> expenseList = expenseService.getExpensesByUserId(userId);
+//    @GetMapping("/{userId}")
+//    public Optional<Expense> getExpensesByUserId(@PathVariable("userId") Long userId) {
+//        Optional<Expense> expenseList = expenseService.getExpensesByUserId(userId);
+//
+//        return expenseList;
+//    }
 
-        return expenseList;
+    @GetMapping("/main")
+    public ResponseEntity<Expense> getExpensesAll(Expense expense) { //나중에 파라미터 USER로 변경
+        Optional<Expense> expenseList = expenseService.getExpensesByUserId(expense.getUserId());
+
+        return expenseList
+                .map(e -> ResponseEntity.ok(e)) //메서드 레퍼런스로 줄인 코드 -> ResponseEntity::ok (Body에 e가 담겨진다)
+                .orElseGet(() -> ResponseEntity.notFound().build()); // 값이 없는 경우 실행되는 코드 (404)
     }
 }
