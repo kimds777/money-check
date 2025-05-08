7f140f65523a74e921787535975450487a6fcb43
kimds777 <77198242+kimds777@users.noreply.github.com>
2025-05-08 23:50:46 +0900

    chore(angular): Angular 로케일을 한국어(ko)로 설정

--- money-check-angular/src/app/app.module.ts
+++ money-check-angular/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeKo from '@angular/common/locales/ko';  // 한국어 로케일 데이터 import
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpenseModule } from './expense/expense.module';
+
+// 한국어 로케일 데이터 등록
+registerLocaleData(localeKo);
 
 @NgModule({
   declarations: [
@@ -14,7 +21,10 @@ import { ExpenseModule } from './expense/expense.module';
     AppRoutingModule,
     ExpenseModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ko-KR' }  // 한국어 로케일 설정
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
