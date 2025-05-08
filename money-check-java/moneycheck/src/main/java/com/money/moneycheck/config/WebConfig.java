f3313d4655f3abef680969449b701be6ac964799
kimds777 <77198242+kimds777@users.noreply.github.com>
2025-05-08 23:47:26 +0900

    fix(java): WebConfig 클래스 수정으로 CORS 이슈 처리

--- money-check-java/moneycheck/src/main/java/com/money/moneycheck/config/WebConfig.java
+++ money-check-java/moneycheck/src/main/java/com/money/moneycheck/config/WebConfig.java
@@ -1,13 +1,18 @@
 package com.money.moneycheck.config;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
 
-public class webConfig implements WebMvcConfigurer {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
-    public void addCorsMapptings(CorsRegistry registry) {
-        registry.addMapping("api/**")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
+
